03b1143b4621c3826b97e99409ef18539282a4e8
Arnthor Agustsson <arnthor3@gmail.com>
2016-12-13 00:39:42 +0000

    build

---
+++ dist/Chart/PathGroup.js
@@ -0,0 +1,138 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _d3Shape = require('d3-shape');
+
+var _d3Selection = require('d3-selection');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/*
+  PropType for fill and stroke..
+ */
+var fillStroke = _react.PropTypes.shape({
+  fill: _react.PropTypes.string,
+  stroke: _react.PropTypes.string
+});
+
+var PathGroup = function (_Component) {
+  _inherits(PathGroup, _Component);
+
+  function PathGroup() {
+    _classCallCheck(this, PathGroup);
+
+    return _possibleConstructorReturn(this, (PathGroup.__proto__ || Object.getPrototypeOf(PathGroup)).apply(this, arguments));
+  }
+
+  _createClass(PathGroup, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var el = (0, _d3Selection.select)(this.container).select('path.val');
+      el.datum([this.props.data.value]);
+      el.node().arc = this.localArc;
+    }
+  }, {
+    key: 'componentDidUpdate',
+    value: function componentDidUpdate(prevProps, prevState) {
+      var el = (0, _d3Selection.select)(this.container).select('path.val');
+      el.datum([this.props.data.value]);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      var marginAndWidth = this.props.pathWidth + this.props.pathMargin;
+      var cX = this.props.width / 2;
+      var cY = this.props.height / 2 - this.props.iter * marginAndWidth;
+      var radius = Math.min(cX, cY);
+      var outer = 0.99 * radius;
+      this.localArc = (0, _d3Shape.arc)().outerRadius(outer).innerRadius(outer - this.props.pathWidth).startAngle(this.props.startAngle).endAngle(this.props.endAngle);
+      return _react2.default.createElement(
+        'g',
+        { transform: 'translate(' + 0 + ',' + this.props.iter * marginAndWidth + ')' },
+        _react2.default.createElement(
+          'g',
+          { transform: 'translate(' + cX + ',' + 0 + ')' },
+          _react2.default.createElement(
+            'text',
+            {
+              textAnchor: 'end',
+              fontSize: this.props.fontSize,
+              fill: this.props.data.fill,
+              dy: marginAndWidth / 2,
+              dx: '-10px',
+              style: {
+                pointerEvents: 'none'
+              }
+            },
+            this.props.data.label
+          )
+        ),
+        _react2.default.createElement(
+          'g',
+          {
+            ref: function ref(c) {
+              _this2.container = c;
+            },
+            transform: 'translate(' + cX + ',' + cY + ')'
+          },
+          _react2.default.createElement('path', {
+            d: this.localArc(),
+            fill: this.props.background.fill,
+            stroke: this.props.background.stroke
+          }),
+          _react2.default.createElement('path', {
+            fill: this.props.data.fill,
+            stroke: this.props.data.stroke,
+            className: 'val'
+          })
+        )
+      );
+    }
+  }]);
+
+  return PathGroup;
+}(_react.Component);
+
+PathGroup.propTypes = {
+  pathWidth: _react.PropTypes.number,
+  pathMargin: _react.PropTypes.number,
+  width: _react.PropTypes.number,
+  height: _react.PropTypes.number,
+  iter: _react.PropTypes.number,
+  startAngle: _react.PropTypes.number,
+  endAngle: _react.PropTypes.number,
+  background: fillStroke,
+  fontSize: _react.PropTypes.string,
+  data: _react.PropTypes.shape({
+    value: _react.PropTypes.number,
+    label: _react.PropTypes.string,
+    fill: _react.PropTypes.string,
+    stroke: _react.PropTypes.string
+  })
+};
+PathGroup.defaultProps = {
+  startAngle: 0,
+  endAngle: Math.PI * 1.5,
+  background: {
+    fill: '#ccc',
+    stroke: '#999'
+  }
+};
+exports.default = PathGroup;
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Chart = require('./Chart/Chart');
+
+var _Chart2 = _interopRequireDefault(_Chart);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _Chart2.default;
